d5722b162cc54c51e02231c87f027d7d63b56470
vishvas savani <vishvassavani456@gmail.com>
2023-09-21 15:38:48 +0530

    Banked Data

---
+++ project1/project1.js
@@ -0,0 +1,5 @@
+sliderbar = document.querySelector(".containar");
+bar_image = document.getElementById("bar-image");
+
+
+
--- project2/product-listing-page.js
+++ project2/product-listing-page.js
@@ -1,18 +1,69 @@
+const irc = document.querySelector(".inside-restaurant-containar");
+
+const getData = async () => {
+
+    const url = "https://savanivishvas.github.io/Javascript-Projects/products.json";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
 
 
-function displaydrop() {
-  var dropdownContent = document.querySelector(".ic-dropdown");
-  var feature = document.querySelector("#f3")
-  
-  feature.addEventListener("click", function(event){
-  if (dropdownContent.style.display === "block") {
-    dropdownContent.style.display = "none";
-  } else {
-    dropdownContent.style.display = "block";
-  }
-});
+    data.products.map((CurrElement) =>{
+
+        const htmldata = `
+
+            <div class="box">
+
+                <div class="inside-box">
+
+                    <div class="ib-d1">
+
+                        <img src="${CurrElement.image}" class="r-img">
+
+                        <p class="discount-txt">${CurrElement.discount}</p>
+
+                    </div>
+
+                    <div class="ib-d2">
+                        <p class="r-txt">${CurrElement['restaurant-name']}</p>
+                        
+                       <div class="rt-box">
+                           <p class="rating">${CurrElement.rating}</p>
+                           <img src="../images/star.svg" class="s-img">
+                        </div>
+                    </div>
+
+                    <div class="ib-d3">
+                        <p class="sub-txt">${CurrElement['sub-name']}</p>
+                        <p class="price">${CurrElement.price}</p>
+                    </div>
+
+
+                    <div class="ib-d4">
+                         <p class="time">${CurrElement['delivery-time']}</p>
+                         <hr class="line">
+                    </div>
+
+                    <div class="ib-d5">
+
+                    <img src="/images/695598f38d29d0e5d3f8ffe57cfdb94c1613145422.webp" class="safety-img"> 
+                        <p class="safety-txt">${CurrElement.safety}</p>  
+                        
+                    </div>
+
+                </div>
+                
+        
+
+        </div>
+        `;
+
+        irc.insertAdjacentHTML('beforeend',htmldata);
+    })
 }
 
-
+getData();
 
 
